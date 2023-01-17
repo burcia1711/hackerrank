46088db70bb8b532d4e9442bfb499490365af99b
Burcu <burcu.alakus@gmail.com>
2023-01-17 08:26:47 +0300

    algorithms easy

---
+++ problem solving/algorithms/migratoryBirds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    newArr=list(set(arr))
+    newArr.sort()
+    counter=[]
+    for i in range(len(newArr)):
+        counter.append(arr.count(newArr[i]))
+    index = counter.index(max(counter))
+    res = newArr[index]
+    return(res)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ problem solving/algorithms/salesByMatch.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    listColor = list(set(ar))
+    numPair = 0
+    for i in range(len(listColor)):
+        numPair += ar.count(listColor[i])//2
+    return numPair
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
